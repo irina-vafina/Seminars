9df6be1494c59584f435f7a6679c468842bf21b6
Irina <arina530@mail.ru>
2022-06-25 19:04:19 +0300

    не полное решение 4 задания

---
+++ Homework1-4/Program.cs
@@ -0,0 +1,12 @@
+﻿int n, current;
+
+Console.Write ("Imput a number:  ");
+n = Convert.ToInt32(Console.ReadLine());
+
+current = 1;
+
+while (current <=n||current%2==0)
+{
+    Console.Write (current+ " ");
+    current ++;  
+}
