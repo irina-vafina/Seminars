fa6e2d6169f4e9e73718f3b2b657bdb746b11ad3
Irina <arina530@mail.ru>
2022-06-25 18:06:22 +0300

    решение 3 задания

---
+++ Homework1-3/Program.cs
@@ -0,0 +1,14 @@
+﻿int num, even;
+
+Console.Write ("Imput a number:  ");
+num = Convert.ToInt32(Console.ReadLine());
+
+even = num/2;
+if (num%2==0)
+{
+    Console.WriteLine ("Yes");
+}
+else 
+{
+    Console.WriteLine ("No");
+}
