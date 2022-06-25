db2e9c949c973b47967d1f30472c5efb24b3a047
Irina <arina530@mail.ru>
2022-06-25 17:18:01 +0300

    решение 2 задания

---
+++ Homework1-2/Program.cs
@@ -0,0 +1,18 @@
+﻿int num1, num2, num3, max;
+
+Console.Write ("Imput a first number:  ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Imput a second number:  ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Imput a third number:  ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+
+max = num1;
+
+if (num1>max) max = num1; 
+if (num2>max) max = num2;
+if (num3>max) max = num3;
+
+Console.WriteLine ("Max = " +max);
+
