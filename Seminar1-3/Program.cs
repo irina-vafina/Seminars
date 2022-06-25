b5d69bcde06327504e5f428ddeb093f5c1047f21
Irina <arina530@mail.ru>
2022-06-25 13:49:04 +0300

    3

---
+++ Seminar1-3/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1, num2, quad;
+
+Console.Write ("Imput a first number:  ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Imput a second number:  ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+quad = num2*num2;
+if (quad == num1 )
+{
+    Console.WriteLine ("First number is quat of secohd number");
+}
+else 
+{
+    Console.WriteLine ("First number is not quat of secohd number");
+}
+
