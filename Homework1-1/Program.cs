ece27f2e2578a406ae07ec6ff4e97035a8ec139e
Irina <arina530@mail.ru>
2022-06-25 17:03:39 +0300

    два варианта решения 1 задания

---
+++ Homework1-1/Program.cs
@@ -0,0 +1,25 @@
+﻿int num1, num2;
+
+Console.Write ("Imput a first number:  ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Imput a second number:  ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+{
+    Console.WriteLine ("First number is max, second number is min");
+}
+else 
+{
+    Console.WriteLine ("First number is min, second number is max");
+}
+
+
+
+
+
+
+
+
+
+
