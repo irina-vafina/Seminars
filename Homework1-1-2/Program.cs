027546d8dff37a214086e0fd01f9ba494e2d1c84
Irina <arina530@mail.ru>
2022-06-25 17:02:44 +0300

    два варианта решения задания 1

---
+++ Homework1-1-2/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1, num2, max;
+
+Console.Write ("Imput a first number:  ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Imput a second number:  ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+max = num1;
+
+if (num1>max) max = num1; 
+if (num2>max) max = num2;
+
+Console.WriteLine ("Max = " +max);
