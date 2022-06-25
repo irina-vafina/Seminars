0d51cf4e6fe97a6ffd255b1aad26f62d49b12a67
Irina <arina530@mail.ru>
2022-06-25 15:54:07 +0300

    6

---
+++ Seminar1-6/Program.cs
@@ -0,0 +1,12 @@
+﻿int n, current;
+
+Console.Write ("Imput a number:  ");
+n = Convert.ToInt32(Console.ReadLine());
+
+current = n*(-1);
+
+while (current <=n)
+{
+    Console.Write (current + " ");
+    current ++;
+}
