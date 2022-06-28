26bd766f123e660a233c94ad1609234eaf874ed8
Irina <arina530@mail.ru>
2022-06-28 08:58:30 +0300

    sem 2-1

---
+++ Sem2-1/Program.cs
@@ -0,0 +1,14 @@
+﻿int CutNumber()
+{
+    int num = new Random().Next(100,1000);
+    Console.WriteLine("Current random number is "+ num);
+    
+    int sotni = num / 100;
+    int ed = num % 10;
+
+    int result = sotni*10 + ed;
+    return result;
+}
+
+int number = CutNumber();
+Console.WriteLine("Result " + number);
