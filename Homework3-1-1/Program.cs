92bb8af36123350947bd83a0971bc0471953f2e7
Irina <arina530@mail.ru>
2022-07-01 19:06:39 +0300

    вариант 2 задания 1

---
+++ Homework3-1-1/Program.cs
@@ -0,0 +1,21 @@
+﻿// 2 вариант решения задачи 1
+
+void Palindrome(int num)
+{
+    num = array [a,b,c,d,e];
+    if(num > 9999 && num < 100000)
+    {
+        if(a = e && b = d) 
+        {
+            Console.WriteLine("Это число является палиндромом");
+        }
+        else
+            Console.WriteLine("Это число не является палиндромом");
+    }
+    else 
+        Console.WriteLine("Ошибка, число не пятизначное");
+}
+
+Console.Write("Введите пятизначное число: ");
+int numPalindr = Convert.ToInt32(Console.ReadLine());
+Palindrome (numPalindr);
