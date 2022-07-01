079f85a10989ce1bf8b99ed83764352c470fe5cb
Irina <arina530@mail.ru>
2022-07-01 17:10:25 +0300

    ДЗ семинар 3

---
+++ Homework3-1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+void Palindrome(int num)
+{
+    if(num > 9999 && num < 100000)
+    {
+        if(num[0] = num[4] && num[1] = num[3]) 
+        {
+            Console.WriteLine("Это число является палиндромом");
+        }
+        else
+            Console.WriteLine("Это число не является палиндромом");
+    }
+    else 
+        Console.WriteLine("Ошибка, число не пятизначное");
+}
+
+Console.Write("Введите пятизначное число: ");
+int numPalinr = Convert.ToInt32(Console.ReadLine());
+Palindrome (numPalinr);
---
+++ Homework3-2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+void СubeNum (int num)
+{
+    int current = 1;
+
+    while(current <= num)
+    {
+        int cube = current*current*current;
+        Console.Write(cube + " ");
+        current ++;
+    }
+}
+
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+СubeNum (n);
