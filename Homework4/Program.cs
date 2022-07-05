f223f49091c44c45b9a1e7f57ea4fc7986a479a0
Irina <arina530@mail.ru>
2022-07-05 09:40:22 +0300

    семинар4

---
+++ Homework4/Program.cs
@@ -0,0 +1,89 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+
+int Numbers(int a) // принимает число и выдает кол-во цифр в числе
+{
+    int i = 0;
+    if(i <= a)
+    {
+        while(a>0)
+        {
+            a=a/10;
+            i= i+1;
+        }
+    }
+    return i;
+}
+
+Console.WriteLine("ВВедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Numbers(n));
+*/
+
+
+
+
+
+
+
+int FindSum(int a) // принимает число, выдает сумму чисел от 1 до А.
+{
+    int sum = 0;
+
+    for (int current = 1; current <= a; current++)
+        sum += current; // sum = sum + current
+    
+    return sum;
+}
+
+Console.WriteLine("ВВедите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма номеров от 1 до {num} есть {FindSum(num)}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Напишите программу, которая задаёт массив из
+// m элементов и выводит их на экран.
+
+/*
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+*/
---
+++ Sem4/Program.cs
@@ -0,0 +1,124 @@
+﻿// сем Напишите программу, которая принимает на вход число
+// (А) и выдаёт сумму чисел от 1 до А.
+/*
+int FindSum(int a)
+{
+    int sum = 0;
+
+    for (int current = 1; current <= a; current++)
+        sum += current; // sum = sum + current
+    
+    return sum;
+}
+
+Console.WriteLine("ВВедите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма номеров от 1 до {num} есть {FindSum(num)}");
+*/
+
+
+// 1 Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+
+//1-1
+/*
+int Kolich(int num)
+{
+    int result = 1;
+    int i = 1;
+    while(num/i>10)
+    {
+        i=i*10;
+        result++;
+    }
+    return result;
+}
+
+Console.WriteLine("ВВедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Kolich(n));
+*/
+
+//1-2
+/*
+int Numbers(int a)
+{
+    int i = 0;
+    if(i <= a)
+    {
+        while(a>0)
+        {
+            a=a/10;
+            i= i+1;
+        }
+    }
+    return i;
+}
+
+Console.WriteLine("ВВедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Numbers(n));
+*/
+
+//2 Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+
+//2-1
+/*
+int FindP(int a)
+{
+    int p = 1;
+
+    for (int current = 1; current <= a; current++)
+        p *= current; // p = p * current
+    
+    return p;
+}
+
+Console.WriteLine("ВВедите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Произведение номеров от 1 до {num} есть {FindSum(num)}");
+*/
+// 2-2
+/*
+int Proisv(int N)
+{
+    int result = 1;
+    for (int i = 1; i <= N; i++)
+        result = result*i; 
+    
+    return result;
+}
+
+Console.WriteLine("ВВедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Proisv(n));
+*/
+
+
+//3 Напишите программу, которая принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+/*
+int FindP(int a, int b)
+{
+    int result = 1;
+
+    for (int i = 0; i <= b; i++)
+    {
+        result = result*a;
+    }
+             
+    return result;
+}
+
+Console.WriteLine("Введите число А: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {num1} в степени {num2} равно {FindP(num1,num2)}");
+*/
