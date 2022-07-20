ca61caf6db360ceeb1955d09edb5a224e5cb8e2b
Irina <arina530@mail.ru>
2022-07-20 23:05:23 +0300

    my head is full of porridge

---
+++ Homework8/Program.cs
@@ -0,0 +1,151 @@
+﻿int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < newArray.GetLength(0); i++)
+        for (int j = 0; j < newArray.GetLength(1); j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] NumbersInOrder(int[,] array)
+{
+    int jMin = 0;   //находим индекс j мин элемента строки
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++) // идем по столбцам чтобы отсортировать в строке
+        {
+            if (array[0, j] < array[0, jMin]) jMin = j;
+        }
+
+            for (int j = 0; j < array.GetLength(1); j++) // перемещаем в начало мин элемент
+            {
+                int temp = array[0, jMin];
+                array[0, jMin] = array[0, j];
+                array[0, j] = temp;
+            }
+    return array;
+
+}
+
+void FindSumRows(int[,] array)
+{
+    int minSum = 0;
+
+    int minSum1=array[0,0]+array[0,1]+array[0,2];
+    int minSum2=array[1,0]+array[1,1]+array[1,2];
+    int minSum3=array[2,0]+array[2,1]+array[2,2];
+    int minSum4=array[3,0]+array[3,1]+array[2,2];
+
+    minSum1 = minSum; Console.WriteLine("Строка 1");
+    if(minSum2<minSum1) minSum2 = minSum; Console.WriteLine("Строка 2");
+    if(minSum3<minSum2) minSum3 = minSum; Console.WriteLine("Строка 3");
+    if(minSum4<minSum3) minSum4 = minSum; Console.WriteLine("Строка 4");
+
+}
+
+
+/*
+//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//1 2 4 7
+//2 3 5 9
+//2 4 4 8
+/*
+Console.Write("Imput number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m,n,min,max);
+Show2dArray(myArray);
+Show2dArray(NumbersInOrder(myArray));
+*/
+
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+/*
+int m = 3;
+int n = 4;
+int min = 0;
+int max = 9;
+
+int[,] myArray = CreateRandom2dArray(m,n,min,max);
+Show2dArray(myArray);
+FindSumRows(myArray);
+
+*/
+
+//Задача 62. Заполните спирально массив 4 на 4.
+//Например, на выходе получается вот такой массив:
+//1 2 3 4
+//12 13 14 5
+//11 16 15 6
+//10 9 8 7
+
+/*
+int [,] array = new int[4,4];
+array[0,0] = 1;
+array[0,1] = 2;
+array[0,2] = 3;
+array[0,3] = 4;
+array[1,0] = 12;
+array[1,1] = 13;
+array[1,2] = 14;
+array[1,3] = 5;
+array[2,0] = 11;
+array[2,1] = 16;
+array[2,2] = 15;
+array[2,3] = 6;
+array[3,0] = 10;
+array[3,1] = 9;
+array[3,2] = 8;
+array[3,3] = 7;
+Console.WriteLine(array);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Sem8/Program.cs
@@ -0,0 +1,228 @@
+﻿//Метод, создающий случайный двумерный массив случайных значений.
+int[,] CreateRandom2dArray1(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < newArray.GetLength(0); i++)
+        for (int j = 0; j < newArray.GetLength(1); j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+//Метод, создающий заданный пользователем двумерный массив.Aij = i+j
+
+int[,] Create2dArray(int rows, int columns)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = i + j;
+
+    return newArray;
+}
+
+
+//Метод выводящий на экран двумерный массив.
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//метод находящий сумму чисел на главной диагонали
+
+int FindDiagSum1(int[,] array)
+{
+    int sum = 0;
+
+    if (array.GetLength(0) == array.GetLength(1))
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (i == j) sum += array[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+//тот же метод после упрощения
+int FindDiagSum(int[,] array)
+{
+    int sum = 0;
+
+    if (array.GetLength(0) == array.GetLength(1))
+        for (int i = 0; i < array.GetLength(0); i++)
+            sum += array[i, i];
+
+    return sum;
+}
+
+//метод находящий элементы, у которых оба индекса чётные,
+//заменяющий эти элементы на их квадраты.
+/*
+int[,] FindQvad(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+
+        for (int j = 0; j < array.GetLength(1); j++)
+
+            if (i % 2 == 0 && j % 2 == 0) array[i, j] = array[i, j] * array[i, j];
+
+    return array;
+}
+
+int[,] newArray = new int[rows,columns];
+for (int i = 0; i < length; i++)
+*/
+
+// метод, заменяет сроки на столбцы, массив должен быть квадрат, иначе сообщ для пользователя
+int[,] Reverse2dArray1(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = i + 1; j < array.GetLength(1); j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+    return array;
+}
+
+
+int[,] Reverse2dArray(int[,] array)
+{
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Number of roms and columns is not the same!");
+        return array;
+    }
+
+    for (int i = 0; i < array.GetLength(0) - 1; i++)
+        for (int j = i + 1; j < array.GetLength(1); j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+
+    return array;
+}
+
+// заполняет 0 строку и столбец на пересеч кот располож мин элемент
+int[,] Cut2dArray(int[,] array)
+{
+    int iMin = 0, jMin = 0;   //находим индекс мин элемента
+
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < array[iMin, jMin])
+            {
+                iMin = i;
+                jMin = j;
+            }
+        }
+
+    for (int j = 0; j < array.GetLength(1); j++) // присваиваем 0 значение элементам в строке и столбце с мин инд
+        array[iMin, j] = 0;                      //элементам с минимальным i, идем по столбцам
+    for (int i = 0; i < array.GetLength(0); i++)
+        array[i, jMin] = 0;                       //элементам с минимальным j, идем по cтрокам
+
+    return array;
+}
+
+
+//меняет местами первую и последнюю строку массива
+int[,] Change2dArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int temp = array[0, j];
+            array[0, j] = array[array.GetLength(0) - 1,j];
+            array[array.GetLength(0) - 1,j] = temp;
+        }
+    return array;    
+}
+
+
+
+
+
+
+//Задача2:
+//Задайте двумерный массив. Напишите программу, которая заменяет строки
+//на столбцы. В случае, если это невозможно, программа должна вывести 
+//сообщение для пользователя.
+/*
+Console.Write("Imput number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m,n,min,max);
+
+Show2dArray(myArray);
+Show2dArray(Reverse2dArray(myArray));
+*/
+//Задача3:
+//Из двумерного массива целых чисел удалить строку и столбец, на 
+//пересечении которых расположен наименьший элемент.
+/*
+Console.Write("Imput number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m,n,min,max);
+Show2dArray(myArray);
+Show2dArray(Cut2dArray(myArray));
+*/
+
+
+//Задача1:
+//Задайте двумерный массив. Напишите программу, которая поменяет местами 
+//первую и последнюю строку массива
+
+
+Console.Write("Imput number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m,n,min,max);
+Show2dArray(myArray);
+Show2dArray(ChangeArray(myArray));                      
